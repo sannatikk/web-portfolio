b207300ce6869bb586fd35877181e6f10b933352
Sanna Tikkakoski <153513791+sannatikk@users.noreply.github.com>
2024-09-10 10:02:15 +0300

    update

---
+++ assets/javascript/actions.js
@@ -0,0 +1,3 @@
+document.getElementById("emailButton").addEventListener("click", function() {
+    window.location.href = 'mailto:t3posa01@students.oamk.fi?subject=Hello%20There&body=I%20found%20your%20test%20website';
+});
